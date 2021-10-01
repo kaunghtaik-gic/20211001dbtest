8307292d726944d82ef40845eb6d9dec5bbeeeb5
kaunghtaik <kaunghtaik@gicjp.com>
2021-10-01 15:19:22 +0900

    change db setting

--- database/connection.js
+++ database/connection.js
@@ -1,21 +1,21 @@
 // Postgres
 const {Client} = require('pg');
 require('dotenv').config();
-  const client = new Client({ 
-    user: 'kh',
-    host: 'aayy70i9w3o612.cmzy7tos3igh.ap-northeast-1.rds.amazonaws.com',
-    database: 'ebdb',
-    password: 'kaunghtaik',
-    port: '5432'
-  });
+  // const client = new Client({ 
+  //   user: 'kh',
+  //   host: 'aayy70i9w3o612.cmzy7tos3igh.ap-northeast-1.rds.amazonaws.com',
+  //   database: 'ebdb',
+  //   password: 'kaunghtaik',
+  //   port: '5432'
+  // });
 
-//   const client = new Client({ 
-//     user: process.env.RDS_USERNAME,
-//     host: process.env.RDS_HOSTNAME,
-//     database: process.env.RDS_DB_NAME,
-//     password: process.env.RDS_PASSWORD,
-//     port: process.env.RDS_PORT
-//   });
+  const client = new Client({ 
+    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB_NAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+  });
 
 //   console.log({
 //     user: process.env.RDS_USERNAME,
