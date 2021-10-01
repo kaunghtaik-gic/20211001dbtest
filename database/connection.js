b892f425f473db605aac20733bf4da3ff21feebf
kaunghtaik <kaunghtaik@gicjp.com>
2021-10-01 15:29:46 +0900

    change db setting

--- database/connection.js
+++ database/connection.js
@@ -17,13 +17,13 @@ require('dotenv').config();
   //   port: process.env.RDS_PORT
   // });
 
-//   console.log({
-//     user: process.env.RDS_USERNAME,
-//     host: process.env.RDS_HOSTNAME,
-//     database: process.env.RDS_DB_NAME,
-//     password: process.env.RDS_PASSWORD,
-//     port: process.env.RDS_PORT
-//  })
+  console.log({
+    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB_NAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+ })
 
 // Postgresと接続する（ローカル）
 // const client = new Client({
