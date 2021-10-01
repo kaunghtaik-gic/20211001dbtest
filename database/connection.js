c3bf2e8ac4394c1393bcbc73d6bbc9ed2280d36e
kaunghtaik <kaunghtaik@gicjp.com>
2021-10-01 17:44:38 +0900

    edit db

--- database/connection.js
+++ database/connection.js
@@ -17,14 +17,6 @@ require('dotenv').config();
   //   port: process.env.RDS_PORT
   // });
 
-  console.log({
-    user: process.env.RDS_USERNAME,
-    host: process.env.RDS_HOSTNAME,
-    database: process.env.RDS_DB_NAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT
- })
-
 // Postgresと接続する（ローカル）
 // const client = new Client({
 //     user:'postgres',
